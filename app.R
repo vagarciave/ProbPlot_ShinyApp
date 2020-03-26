976a139ce8a6008a7cf76195b79cd0e8dc1211f5
vagarciave <vagarciave@unal.edu.co>
2020-03-26 12:12:11 -0500

    Update app.R
    Se agregan bandas de confianza :baby_bottle:

--- app.R
+++ app.R
@@ -5,8 +5,9 @@ library(survival)
 library(SPREDA)
 library(MASS)
 library(actuar)
+library(PearsonDS)
 
-#Function for predicting quantiles
+# Funcion para la predeccion de cuantiles 
 predictData <- function (p, location=0, scale=1,
                          shape=NULL, rate=NULL, dist) {
     if (dist=="weibull") {exp(qsev(p)*scale + location)}
@@ -24,25 +25,25 @@ predictData <- function (p, location=0, scale=1,
 }
 
 
+# Funcion para contruir los graficos de probabilidad:
 
-#Function for constructing probability plots for
-# Datos completos (sin censura) y con censura a derecha
+## Datos completos (sin censura) y con censura a derecha
 ProbPlot <- function (data, event=rep(1, length(data)),
                       weight=rep(1, length(data)), dist,
                       shape=NULL, simplify=TRUE){
-    #-data un vector con los puntos observados.
+    #-data es un vector con los puntos observados.
     #-event es un indicador de estado (0 y 1) censura 0, sin censura 1
     # se omite para datos completos
-    #-weight vector opcional contiene el peso de las observaciones
-    #-dist distribucion asumida. Distribuciones "lognormal",
+    #-weight vector opcional, contiene el peso de las observaciones
+    #-dist es la distribucion asumida. Distribuciones "lognormal",
     # "gaussian", "exponential", "weibull", "sev", "frechet",
-    # "lev", "logistic" ,"loglogistic", "gamma" y "beta"
+    # "lev", "logistic" ,"loglogistic", "gamma" y "beta".
     #-shape vector especificando los parametros de la distribucion
-    #-simplify: if TRUE then do not return a matrix containing the data points
-    # (=data), standardized quantiles (=standardQuantile), and corresponding
-    # probabilities.
+    #-simplify: si es TRUE entonces no devuelve una matriz que contenga
+    # los puntos de datos (=data), los cuantiles estandarizados (=standardQuantile),
+    # y las probabilidades correspondientes.
     
-    #plotting positions
+    # Trazar posiciones
     dataSorted <- data[order(data)]
     eventSorted <- event[order(data)]
     datai <- unique(dataSorted[which(eventSorted==1)])
@@ -53,51 +54,53 @@ ProbPlot <- function (data, event=rep(1, length(data)),
     lagFail <- c(0, cumFail[-length(cumFail)])
     Prob <- .5*(cumFail+lagFail)
     
-    #labels for ticks along y-axis
+    # Etiquetas para las marcas en el eje y
     tick.probs <- c(.001,.005,.01,.02,.05,.1,.2,.3,.4,.5,.6,.7,.8,.9,.99,.999)
     
-    #implement distributions
+    # Implementar las distribuciones
     if (dist=="lognormal" | dist=="gaussian") {
-        yi <- qnorm(Prob) #plotting positions
-        tick.pos <- qnorm(tick.probs) #positions for ticks along y-axis
-        ylimr <- qnorm(range(tick.probs)) #range along y-axis
+        yi <- qnorm(Prob) # Trazar posiciones (cuantiles)
+        tick.pos <- qnorm(tick.probs) # posiciones para las marcas en el eje y
+        ylimr <- qnorm(range(tick.probs)) # rango del eje y
     }
     else if (dist=="exponential") {
-        yi <- qexp(Prob) #plotting positions
-        tick.pos <- qexp(tick.probs) #positions for ticks along y-axis
-        ylimr <- qexp(range(tick.probs)) #range along y-axis
+        yi <- qexp(Prob) # Trazar posiciones (cuantiles)
+        tick.pos <- qexp(tick.probs) # posiciones para las marcas en el eje y
+        ylimr <- qexp(range(tick.probs)) # rango del eje y
     }
     else if (dist=="weibull" | dist=="sev") {
-        yi <- qsev(Prob) #plotting positions
-        tick.pos <- qsev(tick.probs) #positions for ticks along y-axis
-        ylimr <- qsev(range(tick.probs)) #range along y-axis
+        yi <- qsev(Prob) # Trazar posiciones (cuantiles)
+        tick.pos <- qsev(tick.probs) # posiciones para las marcas en el eje y
+        ylimr <- qsev(range(tick.probs)) # rango del eje y
     }
     else if (dist=="frechet" | dist=="lev") {
-        yi <- qlev(Prob) #plotting positions
-        tick.pos <- qlev(tick.probs) #positions for ticks along y-axis
-        ylimr <- qlev(range(tick.probs)) #range along y-axis
+        yi <- qlev(Prob) # Trazar posiciones (cuantiles)
+        tick.pos <- qlev(tick.probs) # posiciones para las marcas en el eje y
+        ylimr <- qlev(range(tick.probs)) # rango del eje y
     }
     else if (dist=="loglogistic" | dist=="logistic") {
-        yi <- qlogis(Prob) #plotting positions
-        tick.pos <- qlogis(tick.probs) #positions for ticks along y-axis
-        ylimr <- qlogis(range(tick.probs)) #range along y-axis
+        yi <- qlogis(Prob) # Trazar posiciones (cuantiles)
+        tick.pos <- qlogis(tick.probs) # posiciones para las marcas en el eje y
+        ylimr <- qlogis(range(tick.probs)) # rango del eje y
     }
     else if (dist=="gamma") {
-        yi <- qgamma(Prob, shape=shape) #plotting positions
-        tick.pos <- qgamma(tick.probs, shape=shape) #positions for ticks along y-axis
-        ylimr <- qgamma(range(tick.probs), shape=shape) #range along y-axis
+        yi <- qgamma(Prob, shape=shape) # Trazar posiciones (cuantiles)
+        tick.pos <- qgamma(tick.probs, shape=shape) # posiciones para las marcas en el eje y
+        ylimr <- qgamma(range(tick.probs), shape=shape) # rango del eje y
     }
     else if (dist=="beta") {
-        yi <- qbeta(Prob, shape1=shape[1], shape2=shape[2]) #plotting positions
-        tick.pos <- qbeta(tick.probs, shape1=shape[1], shape2=shape[2]) #positions for ticks along y-axis
-        ylimr <- qbeta(range(tick.probs), shape1=shape[1], shape2=shape[2]) #range along y-axis
+        yi <- qbeta(Prob, shape1=shape[1], shape2=shape[2]) # Trazar posiciones (cuantiles)
+        tick.pos <- qbeta(tick.probs, shape1=shape[1], shape2=shape[2]) # posiciones para las marcas en el eje y
+        ylimr <- qbeta(range(tick.probs), shape1=shape[1], shape2=shape[2]) # rango del eje y
     }
     
-    #determine range along x-axis
+    # Encontrar el rango del eje x (de los datos)
     rangeData <- range(data)
     
-    #construct plot
-    #distributions that require a log transform of the data scale
+    
+    # Construir el grafico:
+    
+    # distribuciones que requieren una transformación logarítmica de la escala de datos 
     if (dist=="weibull" | dist=="lognormal" | dist=="frechet" |
         dist=="loglogistic") {
         plot(0, type="n", log="x",
@@ -115,86 +118,118 @@ ProbPlot <- function (data, event=rep(1, length(data)),
     axis(2, at=tick.pos, labels=tick.probs)
     axis(1)
     points(datai, yi, col="red")
-    #draw raster
+    # dibujar la trama del fondo
     abline(h = tick.pos, lty=3, col="gray")
     abline(v = axTicks(1), lty=3, col="gray")
     
-    #return plotting positions
+    # retorna las posiciones de trazado
     if (!simplify) cbind(data=datai, standardQuantile=yi, probability=Prob)
 }
 lineas <- function(x, cens=rep(1, length(x)),
                    weight=rep(1, length(x)), dist,
                    shape=NULL, simplify=TRUE){
     ps <- seq(.001, .999, length.out=200)
-    #implement distributions
+    
+    # implementar distribuciones
     if (dist=="lognormal") {
         mleLognormal <- survreg(Surv(x, cens) ~ 1,
                                 weights=weight, dist="lognormal")
         predLognormal <- sapply(ps, predictData, location=coef(mleLognormal),
                                 scale=mleLognormal$scale, dist="lognormal")
-        lines(predLognormal, qnorm(ps), col="blue")
-        
+        lines(predLognormal, qnorm(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predLognormal, qnorm(ps) + 0.45, col="blue", lty = 2)
+        lines(predLognormal, qnorm(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="gaussian"){
         mleNormal <- survreg(Surv(x, cens) ~ 1,
                              weights=weight, dist="gaussian")
         predNormal <- sapply(ps, predictData, location=coef(mleNormal),
                              scale=mleNormal$scale, dist="gaussian")
-        lines(predNormal, qnorm(ps), col="blue")
+        lines(predNormal, qnorm(ps), col="blue")  # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predNormal, qnorm(ps) + 0.45, col="blue", lty = 2)
+        lines(predNormal, qnorm(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="exponential") {
         mleExponential <- fitdistr(x, densfun="exponential")
         predExponential <- sapply(ps, predictData,
                                   scale=1/mleExponential$estimate,
                                   dist="exponential")
-        lines(predExponential, qexp(ps), col="blue")
-        
+        lines(predExponential, qexp(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predExponential, qexp(ps) + 0.45, col="blue", lty = 2)
+        lines(predExponential, qexp(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="weibull") {
         mleWeibull <- survreg(Surv(x, cens) ~ 1,
                               weights=weight, dist="weibull")
         predWeibull <- sapply(ps, predictData, location=coef(mleWeibull),
                               scale=mleWeibull$scale, dist="weibull")
-        lines(predWeibull, qsev(ps), col="blue")
-        
+        lines(predWeibull, qsev(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predWeibull, qsev(ps) + 0.45, col="blue", lty = 2)
+        lines(predWeibull, qsev(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="sev") {
         mleGumbel <- survreg(Surv(x, cens) ~ 1,
                              weights=weight, dist="extreme")
         predGumbel <- sapply(ps, predictData, location=coef(mleGumbel),
                              scale=mleGumbel$scale, dist="sev")
-        lines(predGumbel, qsev(ps), col="blue")
-        
+        lines(predGumbel, qsev(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predGumbel, qsev(ps) + 0.45, col="blue", lty = 2)
+        lines(predGumbel, qsev(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="frechet") {
         mleFrechet <- Lifedata.MLE(Surv(x, cens) ~ 1,
                                    weights=weight, dist="frechet")
         predFrechet <- sapply(ps, predictData, location=coef(mleFrechet)[1],
                               scale=coef(mleFrechet)[2], dist="frechet")
-        lines(predFrechet, qlev(ps), col="blue")
+        lines(predFrechet, qlev(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predFrechet, qlev(ps) + 0.45, col="blue", lty = 2)
+        lines(predFrechet, qlev(ps) - 0.45, col="blue", lty = 2)
         
     }
     else if (dist=="lev") {
-        mleFrechet <- Lifedata.MLE(Surv(x, cens) ~ 1,
+        mleLev <- Lifedata.MLE(Surv(x, cens) ~ 1,
                                    weights=weight, dist="frechet")
-        predFrechet <- sapply(ps, predictData, location=coef(mleFrechet)[1],
-                              scale=coef(mleFrechet)[2], dist="lev")
-        lines(predFrechet, qlev(ps), col="blue")
+        predLev <- sapply(ps, predictData, location=coef(mleLev)[1],
+                              scale=coef(mleLev)[2], dist="lev")
+        lines(predLev, qlev(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predLev, qlev(ps) + 0.45, col="blue", lty = 2)
+        lines(predLev, qlev(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="loglogistic") {
         mleLoglogis <- survreg(Surv(x, cens) ~ 1,
                                weights=weight, dist="loglogistic")
         predLoglogis <- sapply(ps, predictData, location=coef(mleLoglogis),
                                scale=mleLoglogis$scale, dist="loglogistic")
-        lines(predLoglogis, qlogis(ps), col="blue")
+        lines(predLoglogis, qlogis(ps), col="blue")  # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predLoglogis, qlogis(ps) + 0.45, col="blue", lty = 2)
+        lines(predLoglogis, qlogis(ps) - 0.45, col="blue", lty = 2)
     }
     else if (dist=="logistic") {
         mleLogis <- survreg(Surv(x, cens) ~ 1,
                             weights=weight, dist="logistic")
         predLogis <- sapply(ps, predictData, location=coef(mleLogis),
                             scale=mleLogis$scale, dist="logistic")
-        lines(predLogis, qlogis(ps), col="blue") 
+        lines(predLogis, qlogis(ps), col="blue") # Recta de ajuste
+        # bandas de confianza alrededor de la recta
+        lines(predLogis, qlogis(ps) + 0.45, col="blue", lty = 2)
+        lines(predLogis, qlogis(ps) - 0.45, col="blue", lty = 2)
     }
+    
+    else if(dist == "gamma"){
+        
+    }
+    else if(dist == "beta"){
+
+    }
+    
     
 } 
 
@@ -237,13 +272,21 @@ ui <- fluidPage(theme = shinytheme("united"),
                           LEV = "lev",
                           Frechet = "frechet",
                           Logistica = "logistic",
-                          Loglogistica = "loglogistic"
-                          # Gamma = "gamma",
-                          # Beta = "beta"
-                          ),selected = "gaussian")
+                          Loglogistica = "loglogistic",
+                          Gamma = "gamma",
+                          Beta = "beta"
+                          ),selected = "gaussian"),
+            # Panel condicional para distribuciones gamma y beta
+            conditionalPanel(
+                condition = "input.distribucion == 'gamma' | input.distribucion == 'beta'",
+                numericInput("shape1", "Parámetro 1", min=0, value=1),
+                numericInput("shape2", "Parámetro 2", min=0,value=1)
+            )
+        
 
             ),
-        # Datos complentos o con censura a derecha
+
+        # Datos completos o con censura a derecha
 
         
         mainPanel(
@@ -278,6 +321,16 @@ server <- function(input, output) {
         }
         read.csv(infile$datapath)
     })
+    
+    param <- reactive({
+        if(input$distribucion %in% c("gamma","beta")){
+        parametros <- c(input$shape1,input$shape2)
+    }else{
+        parametros <- NULL
+    }
+        return(parametros)
+    })
+
     # Para mostrar los datos cargados
     output$contents = renderDT({
         df <- filedata()
@@ -287,22 +340,27 @@ server <- function(input, output) {
     # Para obtener las posiciones
     output$posiciones = renderDT({
         df <- filedata()
+        parametros <- param()
+        # Obtiene los parametros para beta y gamma
+
         # Verifica el formato de los datos para utilizar ProbPlot
-        if(input$formato == "comp"){ProbPlot(df[,1],dist = input$distribucion,simplify = FALSE)}
+        if(input$formato == "comp"){ProbPlot(df[,1],dist = input$distribucion,simplify = FALSE,
+                                             shape = parametros)}
         else {ProbPlot(df[,1],event = df[,2],
-             dist = input$distribucion,simplify = FALSE)}
+             dist = input$distribucion,simplify = FALSE, shape = parametros)}
     })
     
     # Realiza el grafico de probabilidad
     output$grafico_probabilidad <- renderPlot({
         df <- filedata()
-
+        parametros <- param()
         # Verifica el formato de los datos para utilizar ProbPlot
-        if(input$formato == "comp"){ProbPlot(df[,1],dist = input$distribucion,simplify = FALSE)
-                                    lineas(df[,1],dist = input$distribucion)}
+        if(input$formato == "comp"){ProbPlot(df[,1],dist = input$distribucion,simplify = FALSE,
+                                             shape = parametros)
+                                    lineas(df[,1],dist = input$distribucion,shape = parametros)}
         else{ProbPlot(df[,1],event = df[,2],
-                      dist = input$distribucion,simplify = FALSE)
-                      lineas(df[,1],cens = df[,2],dist = input$distribucion)}
+                      dist = input$distribucion,simplify = FALSE,shape = parametros)
+                      lineas(df[,1],cens = df[,2],dist = input$distribucion,shape = parametros)}
 
     })
     
@@ -311,3 +369,6 @@ server <- function(input, output) {
 
 # Correr la aplicacion
 shinyApp(ui = ui, server = server)
+
+
+
